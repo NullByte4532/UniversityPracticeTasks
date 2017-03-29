9f1d157ac7e1f4e9f427ba597979db6dabeed262
NullByte4532 <y453212@yandex.ru>
2017-03-29 11:38:24 +0300

    removed int f variable from main 'cause it was useless and renamed it to 'state' in process_arg

--- CPU/Asm.cxx
+++ CPU/Asm.cxx
@@ -167,28 +167,28 @@ int process_arg(FILE* fin, FILE* fout, HashTable* htable){
 	ch=fgetc(fin);
 	int val;
 	double dval;
-	int f=0;
+	int state=0;
     while (ch && !isspace(ch) && ch!=0xffffffff) {
 		if (i>=ARG_MAX_LENGTH){
 			fprintf(stderr, "ERROR: Too long argument starting with %s\n", arg); return 0;
 		}
 		arg[i++]=ch;
 		if(isdigit(ch)){
-			if(f==2){
-				f=3;
-			}else if(f==0){ 
-				f=1;
+			if(state==2){
+				state=3;
+			}else if(state==0){ 
+				state=1;
 			}
 		}else if(ch=='.'){
-			if(f==2||f==3){
-				f=4;
-			}else if(f==1){ 
-				f=2;
+			if(state==2||state==3){
+				state=4;
+			}else if(state==1){ 
+				state=2;
 			}else{
-				f=4;
+				state=4;
 			}
 		}else{
-			f=4;
+			state=4;
 		}
 		ch=fgetc(fin);
 		
@@ -196,8 +196,8 @@ int process_arg(FILE* fin, FILE* fout, HashTable* htable){
 	if (arg[0]=='h'&&strlen(arg)<2){
 		fprintf(stderr, "ERROR: Too short argument starting with %s\n", arg); return 0;
 	}
-	if 		(f==3)				{write_file(fout, N_CODE); sscanf(arg,"%lf", &dval); fwrite(&dval, sizeof(double), 1, fout); return 1;}
-	if 		(f==1)				{write_file(fout, NI_CODE); sscanf(arg,"%d", &val); fwrite(&val, sizeof(int), 1, fout); return 1;}
+	if 		(state==3)		   	 {write_file(fout, N_CODE); sscanf(arg,"%lf", &dval); fwrite(&dval, sizeof(double), 1, fout); return 1;}
+	if 		(state==1)			 {write_file(fout, NI_CODE); sscanf(arg,"%d", &val); fwrite(&val, sizeof(int), 1, fout); return 1;}
 	else if	(!strcmp(arg, "EAX")){write_file(fout, R_CODE); write_file(fout, R_EAX); return 1;}
 	else if	(!strcmp(arg, "EBX")){write_file(fout, R_CODE); write_file(fout, R_EBX); return 1;}
 	else if	(!strcmp(arg, "ECX")){write_file(fout, R_CODE); write_file(fout, R_ECX); return 1;}
@@ -280,7 +280,6 @@ int main(int argc, char **argv)
 	FILE* fin;
 	FILE* fout;
 	HashTable htable(32);
-	int f=1;
 	int size, stack_size;
 	fout_name="out.bin";
 	switch(argc-1){
@@ -303,9 +302,7 @@ int main(int argc, char **argv)
 	fwrite(&size, sizeof(int), 1, fout);
 	fwrite(&stack_size, sizeof(int), 1, fout);
 	rewind(fin);
-	while(f){
-		f=process_op(fin, fout, &htable);
-	}
+	while(process_op(fin, fout, &htable)){}
 	return 0;
 }
 
